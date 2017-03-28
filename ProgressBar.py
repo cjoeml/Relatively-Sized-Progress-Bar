f5e7a66852c1e0ed6791a85cda60ac7f0006e89c
cjoe <cjoe@bu.edu>
2017-03-28 16:54:36 -0400

    Changed some documentation

--- ProgressBar.py
+++ ProgressBar.py
@@ -1,14 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
 import time
-import shutil
-##-----------##
-## import os ##
-##-----------##
+import os 
+import sys, subprocess
+##---------------##
+## import shutil ##
+##---------------##
 
-def ProgressBar(items, decimal=2, sleep=0.05):
+def ProgressBar(items, decimal=2, wait=0.05):
 	"""
 	Desc : Create a progress bar with dynamic size
 	Goal : Always fits within current terminal (at time of usage)
-	Reqs : time, shutil [os (deprecated)]
+	Reqs : time, os, sys, subprocess [deprecated: shutil]
 	----------
 	Parameters
 	----------
@@ -19,31 +22,36 @@ def ProgressBar(items, decimal=2, sleep=0.05):
 
 	## Below is deprecated but still can be used for user's own convenience ##
 	##----------------------------------------------------------------------##
-	## r, length = os.popen('stty size', 'r').read().split()			    ##
-	## r, length = subprocess.check_output(['stty', 'size']).split()		##
+	## length = os.popen('stty size', 'r').read().split()[1]				##
+	## length = shutil.get_terminal_size((80, 20))[0] - 30					##
 	##----------------------------------------------------------------------##
 
 	## Build the variables ##
-	length = shutil.get_terminal_size((80, 20))[0] - 30
+	length = subprocess.check_output(['stty', 'size']).split()[1]	
 	fill = "█"
 	finish = len(items)
+	length = int(length) - 30
 
 	## Construct the initial progress bar ##
 	percent = ("{0:." + str(decimal) + "f}").format(100 * (0 / float(finish)))
 	rest = int(length * 0 // finish)
 	progress_bar = fill * rest + '-' * (length - rest)
-	print("\r{0} |{1}| {2}% {3}".format("Status:", progress_bar, percent, "Done"), end='\r')
+	sys.stdout.write("\r{0} |{1}| {2}% {3}\r".format("Status:", progress_bar, percent, "Done"))
 
 	## Updating progress bar ##
 	for i in range(finish+1):
 			percent = ("{0:." + str(decimal) + "f}").format(100 * (i / float(finish)))
 			rest = int(length * i // finish)
 			progress_bar = fill * rest + '-' * (length - rest)
-			print("\r{0} |{1}| {2}% {3}".format("Status:", progress_bar, percent, "Done"), end='\r')
-			time.sleep(sleep)
+			sys.stdout.write("\r{0} |{1}| {2}% {3}\r".format("Status:", progress_bar, percent, "Done"))
+			sys.stdout.flush()
+			time.sleep(wait)
 
 	## End with newline ##
-	print()
+	if sys.version_info < (3, 0):
+		print
+	else:
+		print()
 
 ##============TESTING============##
 ## items = list(range(0, 67))	 ##
