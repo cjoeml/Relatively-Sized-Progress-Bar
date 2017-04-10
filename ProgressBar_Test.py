e5ecb38ca8002a9793b43fbe412f1265e83c6f9a
Chris J <cjoe@bu.edu>
2017-04-10 15:11:36 -0400

    Trivial comment added

--- ProgressBar_Test.py
+++ ProgressBar_Test.py
@@ -1,4 +1,4 @@
 from ProgressBar import ProgressBar
 
-items = list(range(0, 67))
+items = list(range(0, 67)) ## Ideally, items is some kind of data structure you would want to do work in.
 ProgressBar(items)
